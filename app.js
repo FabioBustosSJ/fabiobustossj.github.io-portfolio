f9e8ad7d4c9f8f5d6b41ceff48db22fc5c1b1a2d
FabioBustosSJ <febt2002@hotmail.com>
2023-01-15 15:50:36 -0300

    Actualización código app.js

--- app.js
+++ app.js
@@ -7,3 +7,51 @@ inputs.forEach( input => {
         valida(input.target);
     });
 });
+
+const mensajesDeError = {
+    nombre: {
+        valueMissing: "El campo de nombre no puede estar vacío",
+        patternMismatch: "El nombre solo debe contener letras y espacios, y no debe exceder los 50 caracteres",
+    },
+    correo: {
+        valueMissing: "Este campo no puede estar vacío",
+        typeMismatch: "El correo no es válido"
+    },
+    asunto: {
+        valueMissing: "El campo asunto no puede estar vacío"
+    },
+    mensaje: {
+        valueMissing: "El campo mensaje no puede estar vacío",
+        customError: "El mensaje no debe superar los 300 caracteres",
+    },
+};
+
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+];
+
+
+function mostrarMensajeDeError(tipoDeInput, input) {
+    let mensaje = "";
+    tipoDeErrores.forEach((error) => {
+      if (input.validity[error]) {
+        mensaje = mensajesDeError[tipoDeInput][error];
+      }
+    });
+    return mensaje;
+  }
+
+const validadores = {
+     mensaje: (input) => validarMensaje(input),
+}
+
+function validarMensaje(input) {
+    let mensaje = "";
+    if (mensaje == "") {
+        mensaje = "El campo nombre no debe estar vacío.";
+    }
+    input.setCustomValidity(mensaje)
+}
